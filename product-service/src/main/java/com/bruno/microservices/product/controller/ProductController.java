8ea2cb048ee7dbc298017f5a296c4af18d221267
brazil-bruno <bruno.brazil2022@gmail.com>
2024-04-28 08:55:30 -0300

    createProduct method added

--- product-service/src/main/java/com/bruno/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/bruno/microservices/product/controller/ProductController.java
@@ -1,16 +1,21 @@
 package com.bruno.microservices.product.controller;
 
 import com.bruno.microservices.product.dto.ProductRequest;
+import com.bruno.microservices.product.model.Product;
+import com.bruno.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest) {
-        
+    public Product createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
     }
 }
--- product-service/src/main/java/com/bruno/microservices/product/service/ProductService.java
+++ product-service/src/main/java/com/bruno/microservices/product/service/ProductService.java
@@ -4,19 +4,36 @@ import com.bruno.microservices.product.dto.ProductRequest;
 import com.bruno.microservices.product.model.Product;
 import com.bruno.microservices.product.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest) {
+    public Product createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
                 .price(productRequest.price())
                 .build();
         productRepository.save(product);
+        log.info("Product created successfully");
+        return product;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
